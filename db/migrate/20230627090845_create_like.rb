81fd28bf89b077c26d793cb8112a90d4c7744f90
mohammad mohsen <mohsenhaidari0766@gmail.com>
2023-06-27 13:55:18 +0430

    linters added to the project

--- db/migrate/20230627090845_create_like.rb
+++ db/migrate/20230627090845_create_like.rb
@@ -1,8 +1,8 @@
 class CreateLike < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
-      t.references :user, null: false, foreign_key: {to_table: :users}, index: true
-      t.references :post, null: false, foreign_key: {to_table: :posts}, index: true
+      t.references :user, null: false, foreign_key: { to_table: :users }, index: true
+      t.references :post, null: false, foreign_key: { to_table: :posts }, index: true
 
       t.timestamps
     end
--- db/migrate/20230627091858_create_comment.rb
+++ db/migrate/20230627091858_create_comment.rb
@@ -1,10 +1,10 @@
 class CreateComment < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.references :author, null: false, foreign_key: {to_table: :users}, index: true
-      t.references :post, null: false, foreign_key: {to_table: :posts}, index: true
+      t.references :author, null: false, foreign_key: { to_table: :users }, index: true
+      t.references :post, null: false, foreign_key: { to_table: :posts }, index: true
       t.text :text
-      
+
       t.timestamps
     end
   end
