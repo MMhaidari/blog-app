31b995e66e233d88bbb3e06db39c5bd5407fe2eb
Ritika Rawat <100911738+ritikarawat220@users.noreply.github.com>
2023-07-13 18:58:51 +0530

    Add devise in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-
+devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
+  
   has_many :posts, foreign_key: :author_id, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
